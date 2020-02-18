b459749e8ebe9517931223996d6570cf8bb2bcca
Andres Long <andreslong95@gmail.com>
2020-02-17 23:05:28 -0600

    Dates added and fixed a few bugs

--- assets/script.js
+++ assets/script.js
@@ -7,32 +7,34 @@ let cityHum = $("#city-humidity");
 let cityWind = $("#city-windspeed");
 let cityUV = $("#city-uvindex");
 let cardDeck = $("#deck");
+let currentTime = moment().format("(MM/DD/YYYY)")
 
 let count = 0;
-var cityCount = [];
+let cityCount = [];
 
 //Main Function for a new search query
 function newSearch(){
     //Initializing relevant variables and all the different URLs for different Ajax calls
-    // IDEA: IF STATEMENT FOR IF SEARCH INFO IS NOT JUST EMPTY, THEN EMPTY SEARCH INFO. OTHERWISE USE VALUE OF BUTTON AS CITY
-    var city;
+    let city;
+    //Checking to make sure that there's no repeated entry
     for (k=0; k<cityCount.length;k++){
-        if(search.val() === cityCount[k]){
+        if(search.val().toLowerCase() === cityCount[k]){
             search.val('');
             return;
         }
     }
-    var newCityBtn = $("<li class='btn btn-outline-secondary text-left list-group-item' id='button' data-history='" + count + "'>");
+    let newCityBtn = $("<li class='btn btn-outline-secondary text-left list-group-item' id='button' data-history='" + count + "'>");
     if (search.val() !== ""){
         city = search.val();
         newCityBtn.text(city);
         btnHistory.append(newCityBtn);
-        cityCount = cityCount.concat(search.val());
+        cityCount = cityCount.concat(search.val().toLowerCase());
+        count++;
     } else {
         city = $(this).text();
     }
-    var queryURL= "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=f3e794b6f19299364c3a368c93f4e895";
-    var forecastURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=f3e794b6f19299364c3a368c93f4e895";
+    let queryURL= "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=f3e794b6f19299364c3a368c93f4e895";
+    let forecastURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=f3e794b6f19299364c3a368c93f4e895";
     search.val('');
     console.log($(this).text());
     $.ajax({
@@ -41,15 +43,14 @@ function newSearch(){
     }).then(function(response){
         //Main display
         console.log(response);
-        //IDEA ADD DATAS VALUE TO EACH NEW BUTTON AND HAVE A FOR LOOP COMPARE EVERY DATA VALUE TO NEW SEEARCH FIELD, RETURN IF MATCHES.
-        cityName.text(response.name);
-        var conversion = (response.main.temp - 273.15)*9/5 + 32;
+        cityName.text(response.name + " " + currentTime);
+        let conversion = (response.main.temp - 273.15)*9/5 + 32;
         cityTemp.text(Math.floor(conversion) + "°F");
         cityHum.text(response.main.humidity + "%");
         cityWind.text(response.wind.speed);
-        var latitude = response.coord.lat;
-        var longitude = response.coord.lon;
-        var uvURL = "http://api.openweathermap.org/data/2.5/uvi?appid=f3e794b6f19299364c3a368c93f4e895&lat=" + latitude + "&lon="+ longitude;
+        let latitude = response.coord.lat;
+        let longitude = response.coord.lon;
+        let uvURL = "http://api.openweathermap.org/data/2.5/uvi?appid=f3e794b6f19299364c3a368c93f4e895&lat=" + latitude + "&lon="+ longitude;
         //Making an ajax request for the UV index
         $.ajax({
             url: uvURL,
@@ -64,7 +65,7 @@ function newSearch(){
             }else if (7<res.value){
                 cityUV.attr("class", "bg-danger rounded");
             }
-        })
+        });
     });
     //Emptying the Deck for next iteration
     cardDeck.empty();
@@ -74,24 +75,24 @@ function newSearch(){
     }).then(function(forecast){
         console.log(forecast);
         for (i=0; i<5;i++){
-            var j=(i*7) + i;
+            let j=(i*7) + i;
             //Calling all variables foor the dynamically generated 5 day Forecast Cards
-            var newCityCard = $("<div class = 'card text-white bg-primary' style = 'max-width: 18rem;'>");
-            var newCardBody = $("<div class = 'card-body'>");
-            var cardCity = $("<h5 class = 'card-title'>");
-            var cardIcon;
-            var cardIconCloudSun = $("<p class = 'card-text text-center'><i class='fas fa-cloud-sun'>");
-            var cardIconSun = $("<p class='card-text text-center'><i class='fas fa-sun'>");
-            var cardIconRain = $("<p class='card-text text-center'><i class='fas fa-cloud-showers-heavy'>");
-            var cardIconCloud = $("<p class='card-text text-center'><i class='fas fa-cloud'>");
-            var cardIconCloudSunRain = $("<p class='card-text text-center'><i class='fas fa-cloud-sun-rain'>");
-            var cardIconSnow = $("<p class='card-text text-center'><i class='fas fa-snowflake'>");
-            var classifier = forecast.list[j].weather[0].main;
+            let newCityCard = $("<div class = 'card text-white bg-primary' style = 'max-width: 18rem;'>");
+            let newCardBody = $("<div class = 'card-body'>");
+            let cardDate = $("<h5 class = 'card-title'>");
+            let cardIcon;
+            let cardIconCloudSun = $("<p class = 'card-text text-center'><i class='fas fa-cloud-sun'>");
+            let cardIconSun = $("<p class='card-text text-center'><i class='fas fa-sun'>");
+            let cardIconRain = $("<p class='card-text text-center'><i class='fas fa-cloud-showers-heavy'>");
+            let cardIconCloud = $("<p class='card-text text-center'><i class='fas fa-cloud'>");
+            let cardIconCloudSunRain = $("<p class='card-text text-center'><i class='fas fa-cloud-sun-rain'>");
+            let cardIconSnow = $("<p class='card-text text-center'><i class='fas fa-snowflake'>");
+            let classifier = forecast.list[j].weather[0].main;
             console.log(classifier);
-            var cardTemp = $("<p class = 'card-text'>");
-            var cardHum = $("<p class = 'card-text'>");
+            let cardTemp = $("<p class = 'card-text'>");
+            let cardHum = $("<p class = 'card-text'>");
             //Modifying all variables with the information from the ajax request
-            cardCity.text(forecast.city.name);
+            cardDate.text(forecast.list[j].dt_txt);
             //statements that determine the icon used
             if (classifier === "Clear"){
                 cardIcon = cardIconSun;
@@ -106,10 +107,10 @@ function newSearch(){
             } else {
                 cardIcon = cardIconCloudSunRain;
             }
-            var toF = (forecast.list[j].main.temp - 273.15)*9/5 +32;
+            let toF = (forecast.list[j].main.temp - 273.15)*9/5 +32;
             cardTemp.text("Temp: " + Math.floor(toF) + "°F");
             cardHum.text("Humidity: " + forecast.list[j].main.humidity + "%");
-            newCardBody.append(cardCity);
+            newCardBody.append(cardDate);
             newCardBody.append(cardIcon);
             newCardBody.append(cardTemp);
             newCardBody.append(cardHum);
