4075aa0e7c9ee0aebb8ce547bedb8e42bdebd735
Andres Long <andreslong95@gmail.com>
2020-02-19 10:01:41 -0600

    almost final version

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,4 @@
+//Assigning all of the references to be appended and/or modified
 let search = $("#city-search");
 let searchBtn = $("#button");
 let btnHistory = $("#city-buttons");
@@ -9,7 +10,9 @@ let cityUV = $("#city-uvindex");
 let cardDeck = $("#deck");
 let currentTime = moment().format("(MM/DD/YYYY)")
 
+//Created a count for data indexing using "data-history". This will only activate with the creation of new buttons.
 let count = 0;
+//Initialized an array to store new city information, so as not to repeat similar search results in the button history.
 let cityCount = [];
 
 //Main Function for a new search query
@@ -24,25 +27,29 @@ function newSearch(){
         }
     }
     let newCityBtn = $("<li class='btn btn-outline-secondary text-left list-group-item' id='button' data-history='" + count + "'>");
+    //This conditional statement handles whether the click event comes from a new search term or just an older historic button
+    console.log(typeof(search.val()));
+    console.log(typeof(parseInt(search.val())));
     if (search.val() !== ""){
         city = search.val();
         newCityBtn.text(city);
-        btnHistory.append(newCityBtn);
         cityCount = cityCount.concat(search.val().toLowerCase());
         count++;
     } else {
         city = $(this).text();
     }
+    //URLs for 2 of my ajax requests
     let queryURL= "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=f3e794b6f19299364c3a368c93f4e895";
     let forecastURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=f3e794b6f19299364c3a368c93f4e895";
-    search.val('');
-    console.log($(this).text());
     $.ajax({
         url: queryURL,
         method: "GET",
     }).then(function(response){
         //Main display
-        console.log(response);
+        if (search.val() !== ""){
+            btnHistory.append(newCityBtn);
+        }
+        search.val('');
         cityName.text(response.name + " " + currentTime);
         let conversion = (response.main.temp - 273.15)*9/5 + 32;
         cityTemp.text(Math.floor(conversion) + "°F");
@@ -56,7 +63,6 @@ function newSearch(){
             url: uvURL,
             method: "GET",
         }).then(function(res){
-            console.log(res);
             cityUV.text(res.value);
             if (res.value <= 4){
                 cityUV.attr("class", "bg-success rounded");
@@ -66,6 +72,8 @@ function newSearch(){
                 cityUV.attr("class", "bg-danger rounded");
             }
         });
+    }).catch(function(err){
+        alert("Something went wrong!");
     });
     //Emptying the Deck for next iteration
     cardDeck.empty();
@@ -73,7 +81,6 @@ function newSearch(){
         url: forecastURL,
         method: "GET"
     }).then(function(forecast){
-        console.log(forecast);
         for (i=0; i<5;i++){
             let j=(i*7) + i;
             //Calling all variables foor the dynamically generated 5 day Forecast Cards
@@ -88,7 +95,6 @@ function newSearch(){
             let cardIconCloudSunRain = $("<p class='card-text text-center'><i class='fas fa-cloud-sun-rain'>");
             let cardIconSnow = $("<p class='card-text text-center'><i class='fas fa-snowflake'>");
             let classifier = forecast.list[j].weather[0].main;
-            console.log(classifier);
             let cardTemp = $("<p class = 'card-text'>");
             let cardHum = $("<p class = 'card-text'>");
             //Modifying all variables with the information from the ajax request
@@ -110,6 +116,7 @@ function newSearch(){
             let toF = (forecast.list[j].main.temp - 273.15)*9/5 +32;
             cardTemp.text("Temp: " + Math.floor(toF) + " °F");
             cardHum.text("Humidity: " + forecast.list[j].main.humidity + "%");
+            // Appending everything to its appropriate location
             newCardBody.append(cardDate);
             newCardBody.append(cardIcon);
             newCardBody.append(cardTemp);
